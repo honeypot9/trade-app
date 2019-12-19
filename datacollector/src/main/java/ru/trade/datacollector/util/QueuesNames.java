2aa4d1a4554a9ba0216371d88bd492df956693a3
Denis.Marunich <Denis.Marunich@rt.ru>
2019-12-19 18:49:43 +0300

    Add queue names

--- datacollector/src/main/java/ru/trade/datacollector/util/QueuesNames.java
+++ datacollector/src/main/java/ru/trade/datacollector/util/QueuesNames.java
@@ -2,7 +2,15 @@ package ru.trade.datacollector.util;
 
 public class QueuesNames {
 
-    //init analyze
+    //init calculate chain queue
     public static final String INIT = "init";
 
+    //ext system chain request queue
+    public static final String  EXTSYSTEMCHAINREQ= "extsystemreq";
+
+    //ext system trade couple data request queue
+    public static final String  EXTSYSTEMCOUPLEREQ= "extsystemcouplereq";
+
+    //chain profit calculation queue
+    public static final String  CHAINCALC= "chaincalc";
 }
