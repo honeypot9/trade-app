d75d217401d1a3728ec63091cf96fc12c53ba41f
Denis.Marunich <Denis.Marunich@rt.ru>
2019-12-19 18:49:28 +0300

    Add rest init controller

--- datacollector/src/main/java/ru/trade/datacollector/controller/InitController.java
+++ datacollector/src/main/java/ru/trade/datacollector/controller/InitController.java
@@ -1,14 +1,26 @@
 package ru.trade.datacollector.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rit.pkt.product_inventory_api.dto.InitMessageDto;
 import ru.trade.datacollector_api.controller.InitApi;
 
 
 @RestController
 public class InitController implements InitApi {
-    ResponseEntity<Void> fire(){
+
+    Logger logger = LoggerFactory.getLogger(InitController.class);
+
+    @PostMapping("/init")
+    ResponseEntity<Void> fire(@RequestBody InitMessageDto initMessage){
+        logger.debug("Start InitController.fire()");
+        logger.debug("Payload: "+initMessage.toString());
+        logger.debug("End InitController.fire()");
         return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
