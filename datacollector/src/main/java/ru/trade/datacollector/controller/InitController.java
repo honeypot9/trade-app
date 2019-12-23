7ba0f7dfbcf8d87a7b70e2ba10df47174b423e83
Denis.Marunich <Denis.Marunich@rt.ru>
2019-12-23 19:04:18 +0300

    Add init controller with logging

--- datacollector/src/main/java/ru/trade/datacollector/controller/InitController.java
+++ datacollector/src/main/java/ru/trade/datacollector/controller/InitController.java
@@ -1,14 +1,19 @@
 package ru.trade.datacollector.controller;
 
+import com.fasterxml.jackson.databind.JavaType;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.rit.pkt.product_inventory_api.dto.InitMessageDto;
+import org.springframework.web.context.request.WebRequest;
 import ru.trade.datacollector_api.controller.InitApi;
+import ru.trade.datacollector_api.dto.InitMessageDto;
 
 
 @RestController
@@ -16,11 +21,13 @@ public class InitController implements InitApi {
 
     Logger logger = LoggerFactory.getLogger(InitController.class);
 
+    @Override
     @PostMapping("/init")
-    ResponseEntity<Void> fire(@RequestBody InitMessageDto initMessage){
+    public ResponseEntity<Void> fire(@ApiParam(value = "init msg", required = true) @RequestBody InitMessageDto initMsg) {
         logger.debug("Start InitController.fire()");
-        logger.debug("Payload: "+initMessage.toString());
+        logger.debug("Payload:");
+        logger.debug(initMsg.toString());
         logger.debug("End InitController.fire()");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
