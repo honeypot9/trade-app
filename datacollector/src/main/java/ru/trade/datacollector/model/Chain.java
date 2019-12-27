9da274917db6f3cfa5b40de7b8c4db5aff646d97
Denis.Marunich <Denis.Marunich@rt.ru>
2019-12-27 16:56:25 +0300

    Edit chain data structure

--- datacollector/src/main/java/ru/trade/datacollector/model/Chain.java
+++ datacollector/src/main/java/ru/trade/datacollector/model/Chain.java
@@ -2,6 +2,8 @@ package ru.trade.datacollector.model;
 
 import javax.persistence.*;
 import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "CHAIN")
@@ -10,12 +12,18 @@ public class Chain {
     @SequenceGenerator(name = "CHAIN_SEQ", sequenceName = "CHAIN_SEQ", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CHAIN_SEQ")
     @Column(name = "ID", nullable = false)
-    public int id;
+    public Integer id;
 
     @Column(name = "ALIAS", nullable = true)
-    public String alais;
+    public BigInteger alais;
+
+    @Column(name = "EXTSYSTEM_ID", nullable = true)
+    public Integer extSystemId;
 
     @Column(name = "DESCRIPTION", nullable = true)
     public String description;
+
+    @OneToMany(mappedBy = "CHAIN", orphanRemoval = true)
+    public Set<TradeCouple> tradeCoupleList = new HashSet<TradeCouple>();
 
 }
