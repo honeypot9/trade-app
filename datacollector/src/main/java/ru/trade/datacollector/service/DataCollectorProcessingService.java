51f1cb0aa4954e4599db27979f5f56f0d4b88b9d
Denis.Marunich <Denis.Marunich@rt.ru>
2019-12-27 16:57:12 +0300

    Edit processing service

--- datacollector/src/main/java/ru/trade/datacollector/service/DataCollectorProcessingService.java
+++ datacollector/src/main/java/ru/trade/datacollector/service/DataCollectorProcessingService.java
@@ -1,11 +1,11 @@
 package ru.trade.datacollector.service;
 
-import ru.trade.datacollector_api.dto.InitChainMessageDto;
-import ru.trade.datacollector_api.dto.InitMessageDto;
+
+import ru.trade.datacollector_api.dto.TradeCoupleReqMessageDto;
 
 public interface DataCollectorProcessingService {
 
-    public void initChainProcessing(InitChainMessageDto message);
+    public void initChainProcessing(TradeCoupleReqMessageDto message);
 
 //    public  void tradeChainProcessing(ChainMessageDto message);
 //
